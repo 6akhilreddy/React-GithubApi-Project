63f2f9f2509b238f48207f82495ef94088524e4d
Akhil Reddy <6akhilreddy@gmail.com>
2019-01-22 16:10:59 +0530

    Updates

---
+++ precache-manifest.2e09c4acc5c4893b173772a51898ce00.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1fd9aebc7e394bd05cfb",
+    "url": "/github-user-search/static/js/runtime~main.1fd9aebc.js"
+  },
+  {
+    "revision": "70f2b6a9ecb2ce38a372",
+    "url": "/github-user-search/static/js/main.70f2b6a9.chunk.js"
+  },
+  {
+    "revision": "110c4bee234a97368364",
+    "url": "/github-user-search/static/js/1.110c4bee.chunk.js"
+  },
+  {
+    "revision": "70f2b6a9ecb2ce38a372",
+    "url": "/github-user-search/static/css/main.112ae1bf.chunk.css"
+  },
+  {
+    "revision": "8d0d9ae0e46fb4cd66a42dc6819ef380",
+    "url": "/github-user-search/index.html"
+  }
+];
